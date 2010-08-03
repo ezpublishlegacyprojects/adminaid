2521c6631c698b264476260c160253adf3addb31
aba <info@example.com>
2010-08-03 05:37:01 +0000

    The beginning
    git-svn-id: http://svn.projects.ez.no/adminaid@1 a9c02342-41d6-4ae2-a9ab-3a83a24f6ef0

---
+++ trunk/adminaid/settings/aid.ini.append.php
@@ -0,0 +1,12 @@
+<?php /* #?ini charset="utf-8"?
+
+[Aid]
+# Only users with ID listed here can use user switch function
+UserSwitchIDLimit[]
+UserSwitchIDLimit[]=14
+
+# Classes representing a user
+UserClass[]
+UserClass[]=user
+
+*/ ?>
